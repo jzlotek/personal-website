9ae82f0fbb965fb8ada33e8644620216ac14fded
John Zlotek <jzlotek@gmail.com>
2018-10-29 21:57:35 -0400

    Add basic Vue routing and homepage
    Signed-off-by: John Zlotek <jzlotek@urbn.com>

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -15,18 +15,18 @@ export default {
 </script>
 
 <style>
-html {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #c9c9c9;
-  background: #101b24;
-  height: 100%;
-  width: 100%;
-  margin: 0;
-  padding: 0;
-}
+  html {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #c9c9c9;
+    background: #101b24;
+    height: 100%;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+  }
 
   a {
     text-decoration: none;
